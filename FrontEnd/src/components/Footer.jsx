8f66e8951b069d7da39c2260c3940e5b063e3fa6
Amal Thomas <amalthomas333444@gmail.com>
2024-08-26 20:40:14 +0530

    Updations  Done

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
-      <div className='flex item-center justify-center gap-4'>
-      <Link to='/'>About</Link>
-      <Link to='/'>Contact</Link>
+      <div className='flex item-center justify-center gap-4 '>
+        <p className='font-bold'>CopyRight ⓒ All Rights Are Reserved</p>
+      {/* <Link to='/'>About</Link> */}
+      {/* <Link to='/'>Contact</Link> */}
       </div>
     </footer>
   )
